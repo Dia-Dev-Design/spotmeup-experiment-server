39153647350f2673175ba7410be947f0ad079176
Vxctor11 <victorortiz928@gmail.com>
2024-08-14 11:18:30 -0400

    cretate validation update Transaction

--- routes/validation.routes.js
+++ routes/validation.routes.js
@@ -19,44 +19,71 @@ router.post("/one-time", async (req, res) => {
   }
 });
 
-router.patch("/buy-ticket", async (req, res) => {
-  const { ticketId, validationObjectId } = req.body;
-
+router.put("/update-validation/:transactionId", async (req, res) => {
   try {
-    // Find Validation by ObjectId
-    const validation = await Validation.findById(validationObjectId);
-    if (!validation) {
-      return res.status(404).json({ message: "validation not found" });
-    }
+    const { transactionId } = req.params;
 
-    // Find Ticket by name & price
-    const ticket = await Tickets.findOne({
-      // name: ticketName,
-      // price: ticketPrice,
-      id: ticketId,
-      // validationId: validationObjectId,
-    });
-    if (!ticket) {
-      return res.status(404).json({ message: "Ticket not found" });
-    }
+    const tickets = await Tickets.find({ transaction: transactionId }).populate(
+      { path: "block", populate: "tables", model: "Tables" }
+    );
 
-    // Check if there are enough tickets available
-    if (validation.blocks.quantity <= 0) {
+    if (tickets.length === 0) {
+      return res.status(400).json({
+        success: false,
+        message: "No Ticket found for this transaction.",
+      });
+    }
+    const allActive = tickets.every((ticket) => ticket.status === "active");
+    if (!allActive) {
       return res
         .status(400)
-        .json({ message: "There are not enough tickets available" });
+        .json({ success: false, message: "All tickets are not active." });
+    }
+    const event = tickets[0].event;
+    const layout = tickets[0].layout;
+
+    const validation = await Validation.findOne({ event, layout });
+
+    if (!validation) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Validation not found." });
     }
 
-    // Subtract the number of available tickets
-    validation.blocks.quantity -= 1;
+    tickets.forEach((ticket) => {
+      if (ticket.block) {
+        const blockIndex = validation.blocks.findIndex((block) =>
+          block.blockId.equals(ticket.block._id)
+        );
+
+        if (blockIndex !== -1) {
+          if (ticket.block.tables && ticket.block.tables.length > 0) {
+            const tableIndex = validation.tables.findIndex((table) =>
+              table.tableId.equals(ticket.block.tables._id)
+            );
+            if (tableIndex !== -1) {
+              validation.tables[tableIndex].sold = true;
+            } else {
+              return res
+                .status(400)
+                .json({ success: false, message: "Table not found." });
+            }
+          } else {
+            validation.blocks[blockIndex].quantity -= 1;
+          }
+        } else {
+          return res.status(400).json({
+            success: false,
+            message: "Block not found in validation",
+          });
+        }
+      }
+    });
     await validation.save();
 
-    // Here you can continue with post-purchase processing logic, such as sending a confirmation to the user.
-
-    res.status(200).json({
-      message: "Purchase completed successfully",
-      updatedValidation: validation,
-    });
+    return res
+      .status(200)
+      .json({ success: true, message: "Validation updated successfuly." });
   } catch (error) {
     console.error("Internal Server Error:", error.message);
     return res
@@ -65,7 +92,7 @@ router.patch("/buy-ticket", async (req, res) => {
   }
 });
 
-router.get("/findInEvent/:eventId", async (req, res) => {
+router.get("", async (req, res) => {
   try {
     const { eventId } = req.params;
 
