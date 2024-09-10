d1f25883aacddbc9b18adfa99c2dd3220d937bcf
yankyalvarezm <alvarezyanky7@gmail.com>
2024-09-10 17:42:22 -0400

    change adaptable redirect

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -172,7 +172,7 @@ router.get(
   (req, res) => {
     const { eventId, transactionId } = req.params;
     //redirecting to React App / Client
-    res.redirect(`http://localhost:5173/approved/${eventId}/${transactionId}`);
+    res.redirect(`https://spotmeup-test.netlify.app/approved/${eventId}/${transactionId}`);
   }
 );
 
